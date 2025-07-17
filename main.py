921f9e42b59300ba8fd9f4a98db80d2909265e3c
Jacob Katzeff <jacobkatzeff@gmail.com>
2025-07-17 13:58:11 -0700

    Small fix

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from game_logic import Game, Player, PlayerId, StrategyProfile, Action, Rewards
 import random
 import itertools
 
-strategy_profiles = list(StrategyProfile.__members__.values())
-
-def main():
-    scores = {}
-    rewards = Rewards(AA = {PlayerId.PLAYER_ONE: 3, PlayerId.PLAYER_TWO: 3},
+STRATEGY_PROFILES = list(StrategyProfile.__members__.values())
+GAMES = {
+    "PRISONERS_DILEMMA": Rewards(AA = {PlayerId.PLAYER_ONE: 3, PlayerId.PLAYER_TWO: 3},
                       AB = {PlayerId.PLAYER_ONE: 0, PlayerId.PLAYER_TWO: 5},
                       BA = {PlayerId.PLAYER_ONE: 5, PlayerId.PLAYER_TWO: 0},
-                      BB = {PlayerId.PLAYER_ONE: 1, PlayerId.PLAYER_TWO: 1},)
+                      BB = {PlayerId.PLAYER_ONE: 1, PlayerId.PLAYER_TWO: 1}),
+    "HAWK_DOVE": Rewards(AA = {PlayerId.PLAYER_ONE: 0, PlayerId.PLAYER_TWO: 0},
+                      AB = {PlayerId.PLAYER_ONE: 4, PlayerId.PLAYER_TWO: 1},
+                      BA = {PlayerId.PLAYER_ONE: 1, PlayerId.PLAYER_TWO: 4},
+                      BB = {PlayerId.PLAYER_ONE: 2, PlayerId.PLAYER_TWO: 2}),
+    "COORDINATION": Rewards(AA = {PlayerId.PLAYER_ONE: 4, PlayerId.PLAYER_TWO: 4},
+                      AB = {PlayerId.PLAYER_ONE: 0, PlayerId.PLAYER_TWO: 1},
+                      BA = {PlayerId.PLAYER_ONE: 1, PlayerId.PLAYER_TWO: 0},
+                      BB = {PlayerId.PLAYER_ONE: 1, PlayerId.PLAYER_TWO: 1})
+}
+def print_scores(scores):
+    for item in sorted(scores.items(), key=lambda x: x[1]):
+        print(item[0].name + ": " + str(item[1]))
 
-    for player1_strategy, player2_strategy in itertools.product(strategy_profiles, strategy_profiles):
+def run_tournament(rewards):
+    scores = {}
+
+    for player1_strategy, player2_strategy in itertools.product(STRATEGY_PROFILES, STRATEGY_PROFILES):
         player1 = Player(name='player1', player_id=PlayerId.PLAYER_ONE, strategy_profile=player1_strategy)
         player2 = Player(name='player2', player_id=PlayerId.PLAYER_TWO, strategy_profile=player2_strategy)
         g = Game(rewards, player1, player2)
@@ -22,8 +35,15 @@ def main():
         scores[player1.strategy_profile] = player1_score + g.history.get_score_at(-1, PlayerId.PLAYER_ONE)
         scores[player2.strategy_profile] = player2_score + g.history.get_score_at(-1, PlayerId.PLAYER_TWO)
 
-    for item in sorted(scores.items(), key=lambda x: x[1]):
-        print(item[0].name + ": " + str(item[1]))
+    return scores
 
+def main():
+    coordination_scores = run_tournament(GAMES["COORDINATION"])
+    prisoners_dilemma_scores = run_tournament(GAMES["PRISONERS_DILEMMA"])
+    hawk_dove_scores = run_tournament(GAMES["HAWK_DOVE"])
+
+    combined_scores_fn = lambda k: coordination_scores.get(k, 0) + prisoners_dilemma_scores.get(k, 0) + hawk_dove_scores.get(k, 0)
+    combined_scores = {k: combined_scores_fn(k) for k in STRATEGY_PROFILES}
+    print_scores(combined_scores)
 if __name__ == "__main__":
     main()
